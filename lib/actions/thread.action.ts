7a801dbed42f761cec7c3b728ea429821e535803
Errortime <larb26656@gmail.com>
2023-09-28 00:23:26 +0700

    Feat (add create thread)

---
+++ lib/actions/thread.action.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+  text: string;
+  author: string;
+  comunityId: string | null;
+  path: string;
+}
+export async function createThread({ text, author, comunityId, path }: Params) {
+  try {
+    connectToDB();
+
+    const createdThread = await Thread.create({
+      text,
+      author,
+      comunity: null,
+      path,
+    });
+
+    // Update user model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread: ${error.message}`);
+  }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({ createAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "children",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -47,3 +47,13 @@ export async function updateUser({
     revalidatePath(path);
   }
 }
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user : ${error.message}`);
+  }
+}
